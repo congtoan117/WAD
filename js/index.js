b47bb3ce27a427c8f7161d2397f9960c98c6dcf4
Cong Toan <congtoan972003@gmail.com>
2023-10-06 03:51:30 +0700

    JavaScript Where To

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-var x = 5
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>phạm công toàn</h2>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+</body>
+</html> 
