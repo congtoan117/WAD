5a447e8d977bed93cf1734065382416965f31829
Cong Toan <congtoan972003@gmail.com>
2023-10-08 02:34:02 +0700

    slide 35

--- js/index.js
+++ js/index.js
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>phạm công toàn</h2>
-
-<p id="demo"></p>
-
-<script>
-document.getElementById("demo").innerHTML = "My First JavaScript";
-</script>
-
-</body>
-</html> 
+<!DOCTYPE
